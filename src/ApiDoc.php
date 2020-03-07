49c1b4afb2276b9fa826cafa02ba4f2a287ed119
Omar Rida <omar@realblocks.com>
2020-03-07 14:12:23 -0500

    remove remaining docblocks and add phpintel to .gitignore

--- src/ApiDoc.php
+++ src/ApiDoc.php
@@ -6,30 +6,15 @@ namespace Omarrida\Scribe;
 
 class ApiDoc
 {
-    /**
-     * @var
-     */
     protected $routes;
 
-    /**
-     * @var
-     */
     protected $output;
 
-    /**
-     * ApiDoc constructor.
-     *
-     * @param $documentedRouteCollection
-     */
     public function __construct($documentedRouteCollection)
     {
         $this->routes = $documentedRouteCollection;
     }
 
-    /**
-     * @param array $routes
-     * @return \App\ApiDoc
-     */
     public static function fromRoutes(array $routes): ApiDoc
     {
         $routes = array_map(function ($route) {
@@ -40,17 +25,11 @@ class ApiDoc
     }
 
 
-    /**
-     * @return string
-     */
     public function toMarkdown(): string
     {
         return (new MarkdownFormatter($this))->format();
     }
 
-    /**
-     * @return array
-     */
     public function routes(): array
     {
         return $this->routes;
